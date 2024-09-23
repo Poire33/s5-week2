f284de43f835071e7fe82359a534507c9e211f5f
Rupert spelled with one P <G00423530@atu.ie>
2024-09-23 11:24:10 +0100

    Hello and path variable for name

--- src/main/java/ie/atu/week2/week2/Week2.java
+++ src/main/java/ie/atu/week2/week2/Week2.java
@@ -1,4 +1,20 @@
 package ie.atu.week2.week2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Week2 {
+    @GetMapping("/hello")
+    public String message()
+    {
+        return "Hello";
+    }
+    @GetMapping("/hello/{name}")
+    public String myName(String name)
+    {
+        return "Your Name is " + name;
+    }
+
 }
